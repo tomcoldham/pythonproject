2de9e4feaf1306ce8e08fec02e63ff19732e6750
tomcoldham <>
2023-12-22 16:14:31 +0000

    commentated out functions whilst getting grid to load

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 # function for creating grid
-def create_grid():
-# function for printing grid
-def print_grid():
-# function for placing ships on grid
-def put_ships():
-# function for player guessing ships location
-def player_guess():
+def create_grid(size):
+    return[['-' for _ in range(size)]for _ in range (size)]
 
+# function for printing griddef print_grid():
+
+# function for placing ships on griddef put_ships():
+
+# function for player guessing ships locationdef player_guess():
+size = 5
+
+create_grid(size)
