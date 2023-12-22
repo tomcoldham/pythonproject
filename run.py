3f49672854fc822aef145714bb474cbc9a23e469
tomcoldham <>
2023-12-22 16:20:43 +0000

    tested that grid can be sucessfully shown

--- run.py
+++ run.py
@@ -2,11 +2,14 @@
 def create_grid(size):
     return[['-' for _ in range(size)]for _ in range (size)]
 
-# function for printing griddef print_grid():
+# function for printing grid
+def print_grid(grid):
+    for row in(grid):
+        print(" | ". join(row))
 
 # function for placing ships on griddef put_ships():
 
 # function for player guessing ships locationdef player_guess():
-size = 5
 
-create_grid(size)
+target_grid = create_grid(9)
+print_grid(target_grid)
