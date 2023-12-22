66ceb6f71d426030cb6e5259b8a84b734034088b
tomcoldham <>
2023-12-22 16:08:45 +0000

    added comments to functions to explain thir purpose

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
+# function for creating grid
 def create_grid():
-
+# function for printing grid
 def print_grid():
-
+# function for placing ships on grid
 def put_ships():
-
+# function for player guessing ships location
 def player_guess():
 
